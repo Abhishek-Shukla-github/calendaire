4018bf94a98ca29f37dd865136b2b54aca434294
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2024-09-28 01:14:00 -0400

    Complete Event creation UI and API integration

---
+++ app/(main)/events/page.tsx
@@ -0,0 +1,12 @@
+import CreateEventDrawer from '@/components/create-event'
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>
+        <CreateEventDrawer />
+    </div>
+  )
+}
+
+export default Events
