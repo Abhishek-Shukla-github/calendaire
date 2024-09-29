00af8aa31a3ebd2b9bb181a65d645e5ebe4b5e70
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2024-09-29 02:44:18 -0400

    Complete Dashboard page and cancel meeting feature

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,42 +1,85 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { useUser } from '@clerk/nextjs'
-import React, {useEffect} from 'react';
-import { usernameSchema } from '@/lib/validators'
-import {useForm} from "react-hook-form"
-import {zodResolver} from "@hookform/resolvers/zod"
-import useFetch from '@/hooks/useFetch'
-import { updateUsername } from '@/actions/user'
-import { Loader2 } from 'lucide-react'
-import { BarLoader } from 'react-spinners'
+"use client";
 
-const Dashboard = () => {
-  const {isLoaded, user} = useUser();
+import React, { useEffect } from "react";
+import { useUser } from "@clerk/nextjs";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { updateUsername } from "@/actions/user";
+import { BarLoader } from "react-spinners";
+import useFetch from "@/hooks/useFetch";
+import { usernameSchema } from "@/lib/validators";
+import { getLatestUpdates } from "@/actions/dashboard";
+import { format } from "date-fns";
 
-  const {register, handleSubmit, setValue, formState:{errors}} = useForm({
-    resolver: zodResolver(usernameSchema)
-  })
+export default function DashboardPage() {
+  const { user, isLoaded } = useUser();
 
-  const onSubmit= (data) => {
-    fnUpdateUsername(data.username)
-  }
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(usernameSchema),
+  });
 
   useEffect(() => {
-    setValue("username", user?.username)
-  },[isLoaded])
+    setValue("username", user?.username);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded]);
 
-  const {loading, data,error, fn: fnUpdateUsername} = useFetch(updateUsername)
+  const {
+    loading: loadingUpdates,
+    data: upcomingMeetings,
+    fn: fnUpdates,
+  } = useFetch(getLatestUpdates);
 
+  useEffect(() => {
+    (async () => await fnUpdates())();
+  }, []);
 
+  const { loading, error, fn: fnUpdateUsername } = useFetch(updateUsername);
+
+  const onSubmit = async (data) => {
+    await fnUpdateUsername(data.username);
+  };
 
   return (
-    <main className='space-y-8'>
+    <div className="space-y-8">
       <Card>
         <CardHeader>
-          <CardTitle>Bonjour, {user?.firstName} !</CardTitle>
+          <CardTitle>Welcome, {user?.firstName}!</CardTitle>
+          <p >Here are your upcoming meetings:</p>
         </CardHeader>
+        <CardContent>
+          {!loadingUpdates ? (
+            <div className="space-y-6 font-light">
+              <div>
+                {upcomingMeetings && upcomingMeetings?.length > 0 ? (
+                  <ul className="list-disc pl-5">
+                    {upcomingMeetings?.map((meeting) => (
+                      <li key={meeting.id}>
+                        {meeting.event.title} on{" "}
+                        {format(
+                          new Date(meeting.startTime),
+                          "MMM d, yyyy h:mm a"
+                        )}{" "}
+                        with {meeting.name}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No upcoming meetings</p>
+                )}
+              </div>
+            </div>
+          ) : (
+            <p>Loading updates...</p>
+          )}
+        </CardContent>
       </Card>
 
       <Card>
@@ -44,21 +87,30 @@ const Dashboard = () => {
           <CardTitle>Your Unique Link</CardTitle>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
             <div>
-              <div className='flex items-center gap-2'>
+              <div className="flex items-center gap-2">
                 <span>{window?.location.origin}/</span>
-                <Input {...register("username")} placeholder='Enter Username' />
+                <Input {...register("username")} placeholder="username" />
               </div>
-              {errors.username && <p className='text-red-500 text-sm mt-1'>{errors.username.message}</p>}
+              {errors.username && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.username.message}
+                </p>
+              )}
+              {error && (
+                <p className="text-red-500 text-sm mt-1">{error?.message}</p>
+              )}
             </div>
-            <p>{loading && <BarLoader className='mb-4' width={"100%"}/>}</p>
-            <Button type='submit'>Update Username</Button>
+            {loading && (
+              <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
+            )}
+            <Button type="submit" disabled={loading}>
+              Update Username
+            </Button>
           </form>
         </CardContent>
-        </Card>
-    </main>
-  )
+      </Card>
+    </div>
+  );
 }
-
-export default Dashboard
--- app/(main)/meetings/page.tsx
+++ app/(main)/meetings/page.tsx
@@ -4,7 +4,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import MeetingList from "./_components/meeting_list";
 
 export const metadata = {
-  title: "Your Meetings | Schedulrr",
+  title: "Your Meetings | Calendaire",
   description: "View and manage your upcoming and past meetings.",
 };
 
