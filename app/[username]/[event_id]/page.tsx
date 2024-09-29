e06dfce1163f03df0af4198dae6db9d0fbb48a5c
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2024-09-29 01:20:43 -0400

    Complete Integration with google meet

--- app/[username]/[event_id]/page.tsx
+++ app/[username]/[event_id]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import { getEventDetails } from "@/actions/events";
 import { getEventAvailability } from "@/actions/availability";
 import EventDetails from "./_components/event_details";
+import BookingForm from "./_components/booking-form";
 
 export async function generateMetadata({ params }) {
   const event = await getEventDetails(params.username, params.event_id );
@@ -23,7 +24,7 @@ export async function generateMetadata({ params }) {
 export default async function EventBookingPage({ params }) {
 
   const event = await getEventDetails(params.username, params.event_id);
-  const availability = await getEventAvailability(params.username, params.event_id);
+  const availability = await getEventAvailability(params.event_id);
 
   if (!event) {
     notFound();
@@ -33,6 +34,7 @@ export default async function EventBookingPage({ params }) {
     <div className="flex flex-col justify-center lg:flex-row px-4 py-8">
       <EventDetails event={event} />
       <Suspense fallback={<div>Loading booking form...</div>}>
+      <BookingForm event={event} availability={availability} />
         </Suspense>
     </div>
   );
